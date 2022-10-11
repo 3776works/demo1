edd6c3a90a9a5207ea0259315d0b667e61d684dd
mayumih387 <harabar2@gmail.com>
2022-10-11 10:45:21 +0900

    getip demo

---
+++ pages/demo/getip-demo.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react'
+
+export default function GetIP() {
+  // Prepare a constant `ip` with empty data by default
+  const [ip, setIp] = useState()
+
+  const getIp = async () => {
+    // Connect ipapi.co with fetch()
+    const response = await fetch('https://ipapi.co/json/')
+    const data = await response.json()
+    console.log(data)
+    // Set the IP address to the constant `ip`
+    setIp(data.ip)
+  }
+
+  // Run `getIP` function above just once when the page is rendered
+  useEffect(() => {
+    getIp()
+  }, [])
+
+  return (
+    <p>あなたのIPアドレスは、「{ip}」です。</p>
+  )
+}
